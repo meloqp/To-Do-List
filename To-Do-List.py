c8d77684c9821e285d85f93bf9dd102139e3ca22
meloqp <meloqswork@gmail.com>
2025-07-09 22:35:18 +0300

    demo page layout

--- To-Do-List.py
+++ To-Do-List.py
@@ -1,8 +1,21 @@
 import tkinter as tk
 
+def createTask():
+    newTask = entryField.get()
+    listboxField.insert(0, newTask)
+
 root = tk.Tk()
 root.geometry("500x400")
 root.title("ToDoList")
-button = tk.Button(text="Create task")
-button.pack(fill="x", padx=30)
+
+entryField = tk.Entry()
+entryField.pack(anchor="n", padx=5, fill="x", pady=5)
+
+button = tk.Button(text="New task", borderwidth=1, wraplength=50, command=createTask)
+button.pack(fill="y", pady=5, side="right")
+
+listboxField = tk.Listbox()
+listboxField.pack()
+
 root.mainloop()
+
