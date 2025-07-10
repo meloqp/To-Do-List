520b09a484034b1390388223fdae57127e439f47
meloqp <meloqswork@gmail.com>
2025-07-10 11:18:14 +0300

    Page layout

--- To-Do-List.py
+++ To-Do-List.py
@@ -1,8 +1,12 @@
 import tkinter as tk
+from tkinter import ttk
 
 def createTask():
     newTask = entryField.get()
     listboxField.insert(0, newTask)
+
+def deleteTask():
+    pass
 
 root = tk.Tk()
 root.geometry("500x400")
@@ -11,11 +15,14 @@ root.title("ToDoList")
 entryField = tk.Entry()
 entryField.pack(anchor="n", padx=5, fill="x", pady=5)
 
-button = tk.Button(text="New task", borderwidth=1, wraplength=50, command=createTask)
-button.pack(fill="y", pady=5, side="right")
+buttonCreate = ttk.Button(text="New task", command=createTask)
+buttonCreate.pack(fill="x", pady=1, anchor="n", ipady=1, padx=5)
 
-listboxField = tk.Listbox()
-listboxField.pack()
+buttonDelete = ttk.Button(text="Delete task", command=deleteTask)
+buttonDelete.pack(fill="x", pady=2, anchor="n")
+
+listboxField = tk.Listbox(root, bg="gainsboro")
+listboxField.pack(fill="both", pady=5, padx=5, expand=True)
 
 root.mainloop()
 
